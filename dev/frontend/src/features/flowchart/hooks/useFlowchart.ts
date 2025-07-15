d47b0ca0c213afcf7effe6eb9b6ada9dfbf0f88b
yukihito-jokyu <119581059+yukihito-jokyu@users.noreply.github.com>
2025-07-15 22:59:35 +0900

    :sparkles: ifNodeの使い方を確認

--- dev/frontend/src/features/flowchart/hooks/useFlowchart.ts
+++ dev/frontend/src/features/flowchart/hooks/useFlowchart.ts
@@ -231,24 +231,24 @@ export const useFlowchart = ({ projectId, initialData }: UseFlowchartProps) => {
       },
     ],
     edges: [
-      { id: 'e0-1', source: '0', target: '1' },
-      { id: 'e1-2', source: '1', target: '2' },
-      { id: 'e1-3', source: '1', target: '3' },
-      { id: 'e3-4', source: '3', target: '4' },
-      { id: 'e3-5', source: '3', target: '5' },
-      { id: 'e2-6', source: '2', target: '6' },
-      { id: 'e4-6', source: '4', target: '6' },
-      { id: 'e5-6', source: '5', target: '6' },
-      { id: 'e6-7', source: '6', target: '7' },
-      { id: 'e7-8', source: '7', target: '8' },
-      { id: 'e8-9', source: '8', target: '9' },
-      { id: 'e9-10', source: '9', target: '10' },
-      { id: 'e10-11', source: '10', target: '11' },
-      { id: 'e11-12', source: '11', target: '12' },
-      { id: 'e12-13', source: '12', target: '13' },
-      { id: 'e13-14', source: '13', target: '14' },
-      { id: 'e14-15', source: '14', target: '15' }, // whileループの最終処理→終了へ
-      { id: 'e15-16', source: '15', target: '16' }, // while終了後の処理
+      { id: 'e0-1', source: '0', target: '1', type: 'step', animated: true },
+      { id: 'e1-2', source: '1', target: '2', sourceHandle: 'true', type: 'step', animated: true },
+      { id: 'e1-3', source: '1', target: '3', type: 'step', animated: true },
+      { id: 'e3-4', source: '3', target: '4', sourceHandle: 'true', type: 'step', animated: true },
+      { id: 'e3-5', source: '3', target: '5', type: 'step', animated: true },
+      { id: 'e2-6', source: '2', target: '6', type: 'step', animated: true },
+      { id: 'e4-6', source: '4', target: '6', type: 'step', animated: true },
+      { id: 'e5-6', source: '5', target: '6', type: 'step', animated: true },
+      { id: 'e6-7', source: '6', target: '7', type: 'step', animated: true },
+      { id: 'e7-8', source: '7', target: '8', type: 'step', animated: true },
+      { id: 'e8-9', source: '8', target: '9', type: 'step', animated: true },
+      { id: 'e9-10', source: '9', target: '10', type: 'step', animated: true },
+      { id: 'e10-11', source: '10', target: '11', type: 'step', animated: true },
+      { id: 'e11-12', source: '11', target: '12', type: 'step', animated: true },
+      { id: 'e12-13', source: '12', target: '13', type: 'step', animated: true },
+      { id: 'e13-14', source: '13', target: '14', type: 'step', animated: true },
+      { id: 'e14-15', source: '14', target: '15', type: 'step', animated: true }, // whileループの最終処理→終了へ
+      { id: 'e15-16', source: '15', target: '16', type: 'step', animated: true }, // while終了後の処理
     ],
   };
 
